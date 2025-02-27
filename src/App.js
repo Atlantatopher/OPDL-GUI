4fcc9d7d587316cc39589cc5f255747f7c00d32f
Atlantatopher <tr.topher@gmail.com>
2025-02-27 15:16:15 -0500

    Week 14 update

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ import OPDNavbar from './components/utility/Navbar.js';
 function App() {
 
     const seasonId = "3";
-    const weekNum = "13"
+    const weekNum = "14"
         return (
 
         <div className="App">
